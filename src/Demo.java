9da41b27e8f1c465ec7f12328dfee9453c91c212
Ansh-Patoliya <anshpatoliya1408@gmail.com>
2025-06-09 21:03:51 +0530

    update

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,9 @@
 public class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello, World!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Current number: " + i);
+            Thread.sleep(1000); // Sleep for 1 second
+        }
     }
 }
