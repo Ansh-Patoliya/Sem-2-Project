22ef1a0983234516d3ce6cb3f73f0336c30a6348
mp12github <mudrapatel93@gmail.com>
2025-06-19 19:12:55 +0530

    Testing...

--- src/Demo.java
+++ src/Demo.java
@@ -1,9 +1,6 @@
 public class Demo {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello, World!");
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Current number: " + i);
-            Thread.sleep(1000); // Sleep for 1 second
-        }
+        System.out.println("Testing");
     }
 }
